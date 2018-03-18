c987628b63bf51788de1e3786338fc23d0ebb165
Ryan Hunter <rhunter4@xula.edu>
2018-03-18 02:28:51 -0500

    //TODO Write  [X] randomly generated assignments to the file 'input.dat'.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import javafx.scene.layout.Priority;
-
+import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
@@ -17,7 +16,7 @@ public class Main {
         return super.equals(obj);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("\n\nHello, AssignmentsApp!\n");
 
         //Output the current date-time.
@@ -76,7 +75,7 @@ public class Main {
         System.out.println("\nWithout using a Set, the number of duplicated dates are " + countDuplicates(hundredRandomDates));
 
         //Count the number of evening (after 6pm) dates.
-        ArrayList<LocalDateTime> eveningDates = searchDatesInTimeframe(hundredRandomDates,18, 24 );
+        ArrayList<LocalDateTime> eveningDates = searchDatesInTimeframe(hundredRandomDates, 18, 24);
         System.out.println("\nThe number of evening dates are " + eveningDates.size());
 
         //Count the number of dates in each of the individual 12 months without using a Java Map.
@@ -89,8 +88,8 @@ public class Main {
         }
 
         //Count the number of dates in each of the individual 12 months using a Java Map.
-        if (month >= 1 && month <=12){
-            System.out.println("Using a Java Map, the number of dates in month " + month + " is " + mapByMonthSearch(hundredRandomDates,month));
+        if (month >= 1 && month <= 12) {
+            System.out.println("Using a Java Map, the number of dates in month " + month + " is " + mapByMonthSearch(hundredRandomDates, month));
         }
 
         //Determine the index of the latest LocalDateTime.
@@ -130,6 +129,30 @@ public class Main {
         // Which of assign1, assign2, or assign3 is the earliest?
         System.out.print(whichAssignmentIsEarliest(assign1, assign2, assignment3));
 
+        //TODO Write  [X] randomly generated assignments to the file 'input.dat'.
+        writeRandomAssignments();
+        readRandomAssignments();
+    }
+
+    private static void readRandomAssignments() throws IOException {
+        DataInputStream input = new DataInputStream(new FileInputStream("assignments.dat"));
+        while (input.available() > 0) {
+            int x = input.readInt();
+            System.out.println(x);
+        }
+        input.close();
+    }
+
+    private static void writeRandomAssignments() throws IOException {
+        DataOutputStream output = new DataOutputStream(new FileOutputStream("assignments.dat"));
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+
+            output.writeInt(i);
+            System.out.println(i);
+        }
+        output.close();
     }
 
     private static String whichAssignmentIsEarliest(Assignment assign1, Assignment assign2, Assignment assign3) {
@@ -214,8 +237,8 @@ public class Main {
         return newDate;
     }
 
-    private static String intToOrdinal(int num){
-        String[] suffixes = new String[] {"th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th" };
+    private static String intToOrdinal(int num) {
+        String[] suffixes = new String[]{"th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"};
         //I probably should have made this into a switch/case.
         if (num % 100 == 11 || num % 100 == 12 || num % 100 == 13) {
             return num + "th";
@@ -224,9 +247,9 @@ public class Main {
         }
     }
 
-    private static Integer indexEarliestTime(ArrayList<LocalDateTime> dateList){
+    private static Integer indexEarliestTime(ArrayList<LocalDateTime> dateList) {
         LocalDateTime earliestDateTime = dateList.get(0);
-        for (LocalDateTime date: dateList) {
+        for (LocalDateTime date : dateList) {
             if (earliestDateTime.toLocalTime().isAfter(date.toLocalTime())) earliestDateTime = date;
         }
         return dateList.indexOf(earliestDateTime);
@@ -236,12 +259,12 @@ public class Main {
         return dateList.indexOf(Collections.max(dateList));
     }
 
-    private static Integer mapByMonthSearch (ArrayList<LocalDateTime> dateList, int month){
+    private static Integer mapByMonthSearch(ArrayList<LocalDateTime> dateList, int month) {
         return mapByMonth(dateList).get(month);
     }
 
     private static Map<Integer, Integer> mapByMonth(ArrayList<LocalDateTime> dateList) {
-        Map<Integer,Integer> returnMap = new HashMap<>();
+        Map<Integer, Integer> returnMap = new HashMap<>();
         for (LocalDateTime date : dateList) {
             Integer count = returnMap.get(date.getMonthValue());
             returnMap.put(date.getMonthValue(), (count == null) ? 1 : count + 1);
@@ -251,19 +274,19 @@ public class Main {
 
     private static ArrayList<LocalDateTime> searchByMonth(ArrayList<LocalDateTime> dateList, int month) {
         return (ArrayList) dateList.stream()
-                .filter( date -> date.getMonthValue() == month)
+                .filter(date -> date.getMonthValue() == month)
                 .collect(Collectors.toList());
     }
 
-    private static ArrayList searchDatesInTimeframe (ArrayList<LocalDateTime> dateList, int startHour, int endHour){
+    private static ArrayList searchDatesInTimeframe(ArrayList<LocalDateTime> dateList, int startHour, int endHour) {
         return (ArrayList) dateList.stream()
-                .filter( date -> date.getHour() >= startHour && date.getHour() < endHour)
+                .filter(date -> date.getHour() >= startHour && date.getHour() < endHour)
                 .collect(Collectors.toList());
     }
 
     private static int countDuplicates(ArrayList<LocalDateTime> hundredRandomDates) {
         int count = 0;
-        for (LocalDateTime date: hundredRandomDates) {
+        for (LocalDateTime date : hundredRandomDates) {
             if (Collections.frequency(hundredRandomDates, date) >= 2) count++;
         }
         return count;
@@ -282,11 +305,11 @@ public class Main {
 
     private static ArrayList<LocalDateTime> searchByYear(ArrayList<LocalDateTime> listOfLocalDateTimes, int year) {
         return (ArrayList) listOfLocalDateTimes.stream()
-                .filter( date -> date.getYear()==year)
+                .filter(date -> date.getYear() == year)
                 .collect(Collectors.toList());
     }
 
-    private static ArrayList<LocalDateTime> randomDateArray (int NumElements){
+    private static ArrayList<LocalDateTime> randomDateArray(int NumElements) {
         ArrayList<LocalDateTime> returnArray = new ArrayList<>();
         for (int i = 0; i < NumElements; i++) {
             returnArray.add(randomDateGenerator());
@@ -294,8 +317,8 @@ public class Main {
         return returnArray;
     }
 
-    private static LocalDateTime randomDateGenerator (){
-        long startOfTime = ChronoUnit.MINUTES.between(LocalDateTime.of(0,1,1,0,0),LocalDateTime.now());
+    private static LocalDateTime randomDateGenerator() {
+        long startOfTime = ChronoUnit.MINUTES.between(LocalDateTime.of(0, 1, 1, 0, 0), LocalDateTime.now());
         long minutes = rand.nextInt((int) startOfTime);
         return LocalDateTime.now().minusMinutes(minutes);
     }
